0466880011ec23f9d8dcba16051fadcaf85d345c
kritikakoirala333 <kritikakoirala333@gmail.com>
2025-10-12 16:02:12 +0545

    fixing error

--- src/pages/card.jsx
+++ src/pages/card.jsx
@@ -10,10 +10,15 @@ function CardPage() {
     customername: '',
     mobileno: '',
     address: '',
+    date:'',
     fields: [
       {
+        sn: '',
         name: '',
-        quantity: 0
+        quantity: '',
+        rate: '',
+        amount: '',
+
       }
     ]
   });
@@ -23,16 +28,24 @@ function CardPage() {
       ...prevFormData,
       fields: [
         ...prevFormData.fields,
-        { name: '', quantity: 0 } // new field appended
+        { sn: '', name: '', quantity: '', rate: '', amount: '' } // new field appended
       ]
     }));
   };
+
+  const handleRemoveField = (indexToRemove) => {
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      fields: prevFormData.fields.filter((_, index) => index !== indexToRemove),
+    }));
+  };
+
 
 
   const handleChange = (e, index) => {
     const { name, value } = e.target;
 
-    console.log(name, value, e , index)
+    console.log(name, value, e, index)
     // if index is provided, we are editing a nested field
     if (index !== undefined) {
       setFormData(prev => {
@@ -105,7 +118,7 @@ function CardPage() {
 
 
           <div className="row m-0 p-0 col-12 mt-4">
-            <div className="col-md-4">
+            <div className="col-md-3">
 
               <input
                 type="text"
@@ -116,7 +129,7 @@ function CardPage() {
                 onChange={handleChange}
               />
             </div>
-            <div className="col-md-4">
+            <div className="col-md-3">
 
               <input
                 type="number"
@@ -127,7 +140,7 @@ function CardPage() {
                 onChange={handleChange}
               />
             </div>
-            <div className="col-md-4">
+            <div className="col-md-3">
 
               <input
                 type="text"
@@ -138,12 +151,24 @@ function CardPage() {
                 onChange={handleChange}
               />
             </div>
+             <div className="col-md-3">
+
+              <input
+                type="date"
+                name="date"
+                value={formData.date}
+                className='form-control  p-3 border border-3 rounded-3 '
+                placeholder="Enter Date"
+                onChange={handleChange}
+              />
+            </div>
+
 
           </div>
 
           <div className='card mt-4 p-2'>
             <div className='row' >
-              <div className='col-md-2'>
+              <div className='col-md-1'>
                 <p className='fs-6 fw-bold text-muted'>SN</p>
               </div>
               <div className='col-md-3'>
@@ -152,45 +177,80 @@ function CardPage() {
               <div className='col-md-3'>
                 <p className='fs-6 fw-bold text-muted'>Qty</p>
               </div>
-              <div className='col-md-3'>
+              <div className='col-md-2'>
                 <p className='fs-6 fw-bold text-muted'>Rate</p>
               </div>
-              <div className='col-md-1'>
+              <div className='col-md-2'>
                 <p className='fs-6 fw-bold text-muted'>Amount</p>
 
               </div>
+              <div className='col-md-1'>
+
+              </div>
+             
+
 
             </div>
             {formData.fields.map((field, index) => <>
-              <div className='row' key={index} >
-                <div className='col-md-2'>
-                  <p className='fs-6 fw-bold text-muted'></p>
+              <div className='row mb-2' key={index} >
+                <div className='col-md-1'>
+                  <p>{index + 1}</p>
                 </div>
+
+
                 <div className='col-md-3'>
                   <input
                     type="text"
                     name="name"
                     value={field.name}
                     className='form-control  p-1 border border-2 rounded-3 '
-                    placeholder="Enter Address"
+                    placeholder="Enter name"
                     onChange={(e) => handleChange(e, index)}
                   />
                 </div>
                 <div className='col-md-3'>
                   <input
-                    type="text"
+                    type="number"
                     name="quantity"
                     value={field.quantity}
                     className='form-control  p-1 border border-2 rounded-3 '
-                    placeholder="Enter Address"
+                    placeholder="Enter quantity"
                     onChange={(e) => handleChange(e, index)}
                   />
                 </div>
-                <div className='col-md-3'>
-                  <p className='fs-6 fw-bold text-muted'>Rate</p>
+                <div className='col-md-2'>
+                  <input
+                    type="text"
+                    name="rate"
+                    value={field.rate}
+                    className='form-control p-1 border border-2 rounded-3'
+                    placeholder="Enter rate"
+                    onChange={(e) => handleChange(e, index)}
+                  />
+
+
+                </div>
+                <div className='col-md-2'>
+                  <input
+                    type="text"
+                    name="amount"
+                    value={field.amount}
+                    className='form-control p-1 border border-2 rounded-3'
+                    placeholder="Enter amount"
+                    onChange={(e) => handleChange(e, index)}
+                  />
+
                 </div>
                 <div className='col-md-1'>
-                  <p className='fs-6 fw-bold text-muted'>Amount</p>
+                 
+                  <button
+                    type="button"
+                    className="btn btn-danger btn-sm "
+                     onChange={(e) => handleChange(e, index)}
+                    onClick={() => handleRemoveField(index)}
+                  >
+                    <i className='bi bi-x'></i>
+                  </button>
 
                 </div>
 
@@ -200,9 +260,13 @@ function CardPage() {
           </div>
           {/* ADD FIELD */}
           <button onClick={handleAddField}>Add More</button>
+
+
         </div>
       </div>
       <button className='btn btn-primary px-4 py-2 rounded-3' onClick={saveform}>Save</button>
+
+
     </>
   )
 }
