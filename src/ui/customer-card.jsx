a7b440af7e234508fc76181475e7d7a91d3f6b4d
kritikakoirala333 <kritikakoirala333@gmail.com>
2025-10-13 16:55:09 +0545

    okay

---
+++ src/ui/customer-card.jsx
@@ -0,0 +1,13 @@
+function CustomerCard({customerInfo, handler}) {
+  return <>
+    <div className="row m-0 p-0" onClick={() => handler(customerInfo)}>
+      {/* <div className="col-3 bg-info p-2"></div> */}
+      <div className="col-12 p-2 customerCard">
+        <span className="fw-semibold">{customerInfo.name}</span>
+        <br />
+        <span className="fw-normal">{customerInfo.address}</span>
+      </div>
+    </div>
+  </>
+}
+export default CustomerCard;
