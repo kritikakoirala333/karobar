cd9d653914d13cd95bfa441996d901704ec95583
kritikakoirala333 <kritikakoirala333@gmail.com>
2025-10-17 13:22:06 +0545

    added add product section

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 // Create an Axios instance
 const axiosInstance = axios.create({
-  // baseURL: "http://192.168.1.36:8000/api", // ✅ replace with your actual base URL
-  baseURL: "https://invoicer-backend.azure.com.np/api", 
+  baseURL: "http://192.168.1.36:8000/api", // ✅ replace with your actual base URL
+  // baseURL: "https://invoicer-backend.azure.com.np/api", 
   headers: {
     "Content-Type": "application/json",
   },
